747087994ba07ef269ba5493307ef40b70035563
LuanDezan <112398074+LuanDezan@users.noreply.github.com>
2026-05-27 19:58:46 -0300

    Testando ControladorConta

--- src/Controller/ControladorConta.java
+++ src/Controller/ControladorConta.java
@@ -4,11 +4,45 @@
  */
 package Controller;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Luan
  */
 public class ControladorConta
 {
-    
+    private Configuracao caminhosArquivo = Configuracao_EduardoGiovanniLuan.getInstancia();
+    private ArrayList<Conta> contas = Inicializar.carregarObjetos(caminhosArquivo.getArquivoConta());
+
+    public void criarConta(Conta conta)
+    {
+        contas.add(conta);
+        Fechar.salvarObjetos(contas, caminhosArquivo.getArquivoClientes());
+    }
+
+    public boolean removerConta(int id) 
+    {
+        Conta contaParaRemover = buscarConta(id);
+        if (contaParaRemover != null) {
+
+            contas.remove(contaParaRemover);
+            Fechar.salvarObjetos(contas, caminhosArquivo.getArquivoClientes());
+            
+            return true; // Sucesso na remoção
+        }
+        return false; // Falha na remoção, conta não encontrada
+    }
+
+    public Conta buscarConta(int codConta)
+    {
+        for (Conta conta : contas)
+        {
+            if (conta.getCodConta() == codConta)
+            {
+                return conta;
+            }
+        }
+        return null; // Não encontrou a conta
+    }
 }
