2976eabaf897afcb3cc706f755c5e1934b25db44
HntrZeRo <giovannivaulthunter@gmail.com>
2026-06-02 21:18:52 -0300

    Formulário de cadastro de categorias

--- src/GUI/formularios/cadastroLancamento.java
+++ src/GUI/formularios/cadastroLancamento.java
@@ -4,6 +4,8 @@
  */
 package GUI.formularios;
 
+import Controller.ControladorLancamento;
+
 /**
  *
  * @author Portu
@@ -13,7 +15,7 @@ public class cadastroLancamento extends javax.swing.JDialog {
     /**
      * Creates new form cadastroLancamento
      */
-    public cadastroLancamento(java.awt.Frame parent, boolean modal) {
+    public cadastroLancamento(java.awt.Frame parent, boolean modal, ControladorLancamento ctrlLancamento) {
         super(parent, modal);
         initComponents();
     }
@@ -348,7 +350,7 @@ public class cadastroLancamento extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonCadastrarLancamentoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCadastrarLancamentoActionPerformed
-        try {
+        /*try {
             int codConta = Integer.parseInt(campoCodConta.getText().trim());
             String nomeConta = campoNomeConta.getText().trim();
             String textoSaldo = campoValor.getText().trim().replace(',', '.');
@@ -381,7 +383,7 @@ public class cadastroLancamento extends javax.swing.JDialog {
                 "Erro de conversão",
                 JOptionPane.ERROR_MESSAGE
             );
-        }
+        }*/
     }//GEN-LAST:event_buttonCadastrarLancamentoActionPerformed
 
     private void buttonLimparCamposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLimparCamposActionPerformed
@@ -430,47 +432,7 @@ public class cadastroLancamento extends javax.swing.JDialog {
         // TODO add your handling code here:
     }//GEN-LAST:event_campoDataMaxActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(cadastroLancamento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(cadastroLancamento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(cadastroLancamento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(cadastroLancamento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                cadastroLancamento dialog = new cadastroLancamento(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton buttonCadastrarLancamento;
